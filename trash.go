d96224423de54ba2739da43d6172ea84ae7e31d9
sourya_deepsource <sourya@deepsource.io>
2021-03-17 22:33:14 +0530

    rancher -> deepsourcelabs
    Signed-off-by: sourya_deepsource <sourya@deepsource.io>

--- trash.go
+++ trash.go
@@ -19,8 +19,8 @@ import (
 	"github.com/urfave/cli"
 
 	"github.com/Masterminds/glide/godep"
-	"github.com/rancher/trash/conf"
-	"github.com/rancher/trash/util"
+	"github.com/deepsourcelabs/trash/conf"
+	"github.com/deepsourcelabs/trash/util"
 	"gopkg.in/yaml.v2"
 )
 
